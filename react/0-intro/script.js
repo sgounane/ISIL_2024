f92b2047a78e9a66d98f5db92c05147d7ab28d06
gounane <gounane.said@gmail.com>
2023-12-05 15:38:02 +0100

    fin exemple 0

--- react/0-intro/script.js
+++ react/0-intro/script.js
@@ -1,3 +1,9 @@
-let app=React.createElement("h1",{},"Bonjour tous!!!!");
+let stl={
+    backgroundColor:"orange",
+    border:"1px solid black",
+    padding:"10px",
+    margin:"10px"
+}
+let app=React.createElement("h1",{style:stl},"Bonjour tous!!!!");
 const root=document.querySelector("#root")
 ReactDOM.render(app,root)
